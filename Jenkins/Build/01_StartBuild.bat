73bcf6ea13a0bdd36caa4cc0e886c24b02a7b403
Lukas Zanner <zannix@freenet.de>
2019-06-05 20:42:16 +0200

    Add directory creation on build

--- Jenkins/Build/01_StartBuild.bat
+++ Jenkins/Build/01_StartBuild.bat
@@ -1,1 +1,4 @@
+if not exist "%JENKINS_WS%" (
+    mkdir "%JENKINS_WS%"
+)
 "%UE_LOC%\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="%JENKINS_WS%\GetaGameJam7.uproject" -game -rocket -progress
